b9ec4519a3091896cdfa9017f7fa92d5d6ad0554
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-06-06 23:44:17 -0700

    toggle button styling completed and function

--- src/component/ToggleButton.js
+++ src/component/ToggleButton.js
@@ -4,13 +4,16 @@ function ToggleButton() {
   };
   return (
     <div className="flex items-center">
-      <p className="pr-3.5">Dark Mode</p>
-      <button
-        onClick={toggleTheme}
-        className="py-5 px-3 rounded bg-black text-white hover:bg-slate-800"
-      >
-        button
-      </button>
+      <label class="inline-flex items-center cursor-pointer">
+        <input type="checkbox" value="" class="sr-only peer" />
+        <span class="mr-3 text-sm font-medium text-gray-900 dark:text-white">
+          Dark Mode
+        </span>
+        <div
+          class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-gray-900 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white peer-checked:after:bg-gray-900 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-tr from-sky-500 to-green-500"
+          onClick={toggleTheme}
+        ></div>
+      </label>
     </div>
   );
 }
