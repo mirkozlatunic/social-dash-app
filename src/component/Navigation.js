1118ea6c30c29296d093da54fbf48d530292eed0
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-06-12 22:47:22 -0700

    work in progress with mobile version

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -2,7 +2,7 @@ import ToggleButton from "./ToggleButton";
 
 function Navigation() {
   return (
-    <nav className="flex justify-between px-40 pt-9 pb-11 dark:bg-slate-900 bg-slate-50">
+    <nav className="sm:flex sm:flex-row sm:justify-between sm:px-40 px-6 pt-9 pb-11 dark:bg-slate-900 bg-slate-50">
       <div>
         <h1 className="text-xl font-bold">Social Media Dashboard</h1>
         <p className="text-slate-500 font-bold text-sm">
--- src/component/ToggleButton.js
+++ src/component/ToggleButton.js
@@ -10,7 +10,7 @@ function ToggleButton() {
           Dark Mode
         </span>
         <div
-          class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-gray-900 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white peer-checked:after:bg-gray-900 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-tr from-sky-500 to-green-500"
+          class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-gray-900 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white peer-checked:after:bg-gray-900 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gradient-to-tr from-sky-500 to-green-500 hover:bg-gradient-to-tr from-sky-500 to-green-500"
           onClick={toggleTheme}
         ></div>
       </label>
